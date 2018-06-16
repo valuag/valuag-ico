7ece97cd7b0b0def24559926d86cef4e495ce452
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-06-15 21:33:52 -0400

    remove themify-icons from source code

--- src/components/app-mission-area/app-mission-area.tsx
+++ src/components/app-mission-area/app-mission-area.tsx
@@ -14,21 +14,21 @@ export class AppMissionArea {
             <div class="row h-100 align-items-center">
               <div class="col-12 col-md-4">
                 <div class="single-mission wow fadeInUp" data-wow-delay="0.2s">
-                  <i class="ti-shine"></i>
+                  <i class="ti ti-shine"></i>
                   <h6>Quality</h6>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, illo, cumque. Fuga neque similique quibusdam animi ipsam obcaecati.</p>
                 </div>
               </div>
               <div class="col-12 col-md-4">
                 <div class="single-mission wow fadeInUp" data-wow-delay="0.3s">
-                  <i class="ti-ruler-pencil"></i>
+                  <i class="ti ti-ruler-pencil"></i>
                   <h6>Creativity</h6>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, illo, cumque. Fuga neque similique quibusdam animi ipsam obcaecati.</p>
                 </div>
               </div>
               <div class="col-12 col-md-4">
                 <div class="single-mission wow fadeInUp" data-wow-delay="0.4s">
-                  <i class="ti-heart"></i>
+                  <i class="ti ti-heart"></i>
                   <h6>Reliability</h6>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, illo, cumque. Fuga neque similique quibusdam animi ipsam obcaecati.</p>
                 </div>
